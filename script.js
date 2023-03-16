33a512961008733f85b5e1c69c3638ea71a7c3fa
Joris Slagter <joris@trivetechnology.com>
2023-03-16 11:34:40 +0100

    We hebben een JavaScript-script geschreven dat het volgende doet: 1. Het vraagt de gebruiker om een PDF-bestand te kiezen. 2. Het leest het gekozen PDF-bestand in en laadt het in het geheugen. 3. Het bepaalt het aantal pagina's in het document. 4. Het maakt een nieuwe map aan om de pagina's op te slaan. 5. Het loopt door elke pagina in het document en maakt een nieuw PDF-document met alleen deze pagina. 6. Het slaat het nieuwe PDF-document op als een apart bestand in de nieuwe map. 7. Het geeft een bericht weer als het klaar is. 8. Dit script kan worden uitgevoerd op de opdrachtregel en is in staat om elk PDF-bestand dat de gebruiker kiest, automatisch op te splitsen in afzonderlijke pagina's.
    We hebben ook de functionaliteit toegevoegd om de gebruiker een lijst van PDF-bestanden in de huidige map te laten zien en hen een keuze te laten maken met behulp van een prompt. Dit maakt het gemakkelijker voor de gebruiker om het PDF-bestand te kiezen dat ze willen splitsen.
    
    Ten slotte hebben we de gebruiker de mogelijkheid gegeven om het script opnieuw te starten nadat het klaar is met het splitsen van een PDF-bestand. Dit is handig als de gebruiker meerdere PDF-bestanden wil splitsen zonder het script telkens opnieuw te hoeven starten.
    
    In latere commits hebben we extra functies toegevoegd, zoals het opslaan van de gesplitste pagina's in een nieuwe map met een unieke naam en het toevoegen van de mogelijkheid om de bestandsnaam als parameter via de opdrachtregel door te geven.

---
+++ script.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+const readline = require("readline");
+const { PDFDocument } = require("pdf-lib");
+const inquirer = require("inquirer");
+
+async function promptUser() {
+  // vraag de gebruiker om een keuze
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "Wat wil je doen?",
+      choices: [
+        { name: "Split PDF", value: "split" },
+        { name: "Stoppen", value: "stop" },
+      ],
+    },
+  ]);
+
+  // als de gebruiker kiest om te stoppen, stop dan het script
+  if (answer.action === "stop") {
+    console.log("Tot ziens!");
+    return;
+  }
+
+  // als de gebruiker kiest om PDF's te splitsen, vraag dan om een bestandsnaam
+  const pdfFiles = fs
+    .readdirSync(process.cwd())
+    .filter((filename) => filename.toLowerCase().endsWith(".pdf"));
+
+  // als er geen PDF-bestanden zijn gevonden, toon dan een foutmelding en stop het script
+  if (pdfFiles.length === 0) {
+    console.error("Er zijn geen PDF-bestanden gevonden in de huidige map.");
+    return;
+  }
+
+  // maak een lijst met keuzes voor de prompt
+  const choices = pdfFiles.map((filename) => ({
+    name: filename,
+    value: filename,
+  }));
+
+  // vraag de gebruiker om een bestandsnaam
+  const answer2 = await inquirer.prompt([
+    {
+      type: "list",
+      name: "filename",
+      message: "Kies een PDF-bestand om op te splitsen:",
+      choices,
+    },
+  ]);
+
+  // splits de PDF
+  await splitPDF(answer2.filename);
+
+  // herstart de prompt
+  await promptUser();
+}
+
+async function splitPDF(filename) {
+  // lees het PDF-bestand in
+  const pdfBytes = fs.readFileSync(filename);
+
+  // laad het PDF-bestand in het geheugen
+  const pdfDoc = await PDFDocument.load(pdfBytes);
+
+  // haal het aantal pagina's in het document op
+  const numPages = pdfDoc.getPageCount();
+
+  // genereer een unieke mapnaam op basis van de bestandsnaam en de huidige datum en tijd
+  const baseFilename = filename.replace(".pdf", "");
+  const folderName = `${baseFilename} - ${new Date()
+    .toISOString()
+    .replace(/:/g, "-")}`;
+  fs.mkdirSync(folderName);
+
+  // loop door elke pagina in het document
+  for (let i = 0; i < numPages; i++) {
+    // maak een nieuw PDF-document met alleen deze pagina
+    const newDoc = await PDFDocument.create({ preservePDFEmbedding: true });
+
+    const [copiedPage] = await newDoc.copyPages(pdfDoc, [i]);
+    newDoc.addPage(copiedPage);
+
+    // schrijf het nieuwe PDF-document naar een bestand
+    const pageNumber = i + 1;
+    const pageFilename = `${folderName}/${baseFilename} - Page ${pageNumber} - ${new Date()
+      .toISOString()
+      .replace(/:/g, "-")}.pdf`;
+    const pdfBytes = await newDoc.save();
+    fs.writeFileSync(pageFilename, pdfBytes);
+  }
+
+  console.log("Done!");
+}
+
+// start het script
+promptUser();
